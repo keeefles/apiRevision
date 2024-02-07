8313f00add692f6a353ab6be6a1e11aba3924001
Aakeefah Jones <aakeefahj@gmail.com>
2024-02-07 16:18:54 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello there!");
+import {addition} from './addition.js'
+// import {subtraction} from './subtraction.js'
+
+console.log(addition(3, 6));
+// console.log(subtraction(3, 6));
+// console.log(subtraction(7, 6));
